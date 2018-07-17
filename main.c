d783342c80e4f2a37cdd3e5e7cc5be31b1286e05
Bastian Santander <bsantander@users.noreply.github.com>
2018-07-17 17:11:42 -0400

    v0.01

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Headers/Funciones/funciones.h"
+#include "Headers/Funciones/Cabecera_funciones.c"
 #include "Headers/Tipos/tablaHash.h"
+#include "Headers/Estructuras/Estructuras.h"
+
 
 int main()
 {
